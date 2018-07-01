4c6f9a0a905c45308878d3c615b88f1f6c581063
SHASHWAT SRIVASTAVA <shash.knuth.srivastava@gmail.com>
2018-07-01 13:45:10 +0530

    Initial Commit

---
+++ src/main/java/com/footballArena/footballersarena/FootballersArenaApplication.java
@@ -0,0 +1,12 @@
+package com.footballArena.footballersarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballersArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballersArenaApplication.class, args);
+	}
+}
